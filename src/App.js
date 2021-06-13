bf948f93f30a9b8c46a003352dbabf9ad95e1079
alexandermakhorin <alnikmakh@gmail.com>
2021-06-13 17:18:02 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [weather, setWeather] = useState([]);
+  const fetchWeather = async () => {
+    return await (await fetch("http://localhost:8000/weather")).json();
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchWeather();
+    setWeather(data);
+    }
+    fetchData();
+    
+  }, []);
+
+  const clickHandler = () => {
+    console.log(weather);
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div>
+        <button type="button" onClick={clickHandler} >Touch me</button>
+      </div>
     </div>
   );
 }
